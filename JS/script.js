54a0493f5d7c53850ae45abfde370e4ba4f62012
SandeepKaur84 <kaur190sandeep@gmail.com>
2026-06-23 12:48:21 +0530

    JS basics

---
+++ JS/script.js
@@ -0,0 +1,67 @@
+// console.log("Helloo World!");
+// console.log("brjbgjuew");
+// console.log(5*8);
+
+//Number
+// a = 10; 
+
+//String
+// name = "abc";
+
+//Print
+// console.log(name);
+// console.log(a)
+
+//Variable - to store any data
+//1. var -> redeclare + reassign
+//2. let -> reassign
+//3. const
+
+// var a = 20;
+// var a = 50;
+// var a = 60;
+// console.log(a);
+
+// let b = 30;
+// b = 40;
+// console.log(b);
+
+// const c = 40;
+// const pi = 3.14;
+// console.log(pi);
+
+// let _name =  "webf";
+// console.log(_name);
+
+// let $name = "euf";
+// console.log($name);
+
+//Number
+let num = 5.87;
+
+//String
+let gender = "female";
+let name = 'Sandeep';
+
+//Data Types - > 1. primitive 2. Non-primitive
+//number, string, boolean , null , undefined
+// functions , arrays , objects
+
+//boolean -> true / false
+let isValid = false;
+console.log(isValid);
+
+//undefined
+let x;
+console.log(x);
+
+//null
+let y = null;
+console.log(y);
+
+console.log(typeof(num));
+console.log(typeof(gender));
+console.log(typeof(isValid));
+console.log(typeof(x));
+console.log(typeof(y));
+
